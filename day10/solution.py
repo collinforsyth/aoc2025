011430be88b742d3738d7eedf2bd3f1322c1282d
Collin <collin.forsyth@hinge.co>
2025-12-10 13:44:56 -0500

    part 1

--- day10/solution.py
+++ day10/solution.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dataclasses import dataclass
 import re
+from collections import deque
 
 CURR_DIR = Path(__file__).parent
 INPUT_PATH = CURR_DIR / "input.txt"
@@ -10,31 +11,58 @@ INPUT_PATH = CURR_DIR / "input.txt"
 class Machine:
     indicators: str
     schematic: list[list[int]]
-    joltages: list[int]
+    joltages: tuple[int, ...]
 
 
 def parse_input(text: str) -> list[Machine]:
-    machines : list[Machine] = []
+    machines: list[Machine] = []
     for line in text.splitlines():
-        pattern = r'\[(.*?)\](.*)\{(.*?)\}'
+        pattern = r"\[(.*?)\](.*)\{(.*?)\}"
         match = re.match(pattern, line)
         if match:
-            joltages = [int(v) for v in match.group(3).split(',')]
-            schematic_group = re.findall(r'\(([^)]+)\)', line)
-            schematics = [[int(x) for x in group.split(',')] for group in schematic_group]
-            machines.append(Machine(match.group(1), schematics, joltages))
+            joltages = [int(v) for v in match.group(3).split(",")]
+            schematic_group = re.findall(r"\(([^)]+)\)", line)
+            schematics = [
+                [int(x) for x in group.split(",")] for group in schematic_group
+            ]
+            machines.append(Machine(match.group(1), schematics, tuple(joltages)))
     return machines
 
 
 def part1(machines: list[Machine]) -> int:
+    fewest_buttons = 0
     for machine in machines:
-        initial_state = ['.' * len(machine.indicators)]
-        print(initial_state)
-        print(machine)
-    return 0
+        initial_state = "." * len(machine.indicators)
+        visited: dict[str, int] = {}
+        queue: deque[str] = deque()
+
+        queue.append(initial_state)
+        visited[initial_state] = 0
+        found = False
+        while queue and not found:
+            state = queue.popleft()
+            for transition in machine.schematic:
+                s = []
+                for i, c in enumerate(state):
+                    if i in transition:
+                        if c == ".":
+                            s.append("#")
+                        else:
+                            s.append(".")
+                    else:
+                        s.append(c)
+                new_state = "".join(s)
+                if new_state == machine.indicators:
+                    fewest_buttons += visited[state] + 1
+                    found = True
+                    break
+                if new_state not in visited:
+                    visited[new_state] = visited[state] + 1
+                    queue.append(new_state)
+    return fewest_buttons
 
 
-def part2(input: list[Machine]) -> int:
+def part2(machines: list[Machine]) -> int:
     return 0
 
 
--- day10/solution_test.py
+++ day10/solution_test.py
@@ -9,9 +9,9 @@ EXAMPLE_INPUT = """\
 
 def test_part1_example():
     data = parse_input(EXAMPLE_INPUT)
-    assert part1(data) == 50
+    assert part1(data) == 7
 
 
 def test_part2_example():
     data = parse_input(EXAMPLE_INPUT)
-    assert part2(data) == 24
+    assert part2(data) == 33
