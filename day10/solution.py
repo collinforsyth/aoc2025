c5df718636ec67195efec336ce903a0419720610
Collin <collin.forsyth@hinge.co>
2025-12-10 17:02:12 -0500

    day 10 :(

--- day10/solution.py
+++ day10/solution.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from dataclasses import dataclass
 import re
 from collections import deque
+import z3
 
 CURR_DIR = Path(__file__).parent
 INPUT_PATH = CURR_DIR / "input.txt"
@@ -11,7 +12,7 @@ INPUT_PATH = CURR_DIR / "input.txt"
 class Machine:
     indicators: str
     schematic: list[list[int]]
-    joltages: tuple[int, ...]
+    joltages: list[int]
 
 
 def parse_input(text: str) -> list[Machine]:
@@ -25,7 +26,7 @@ def parse_input(text: str) -> list[Machine]:
             schematics = [
                 [int(x) for x in group.split(",")] for group in schematic_group
             ]
-            machines.append(Machine(match.group(1), schematics, tuple(joltages)))
+            machines.append(Machine(match.group(1), schematics, joltages))
     return machines
 
 
@@ -62,8 +63,41 @@ def part1(machines: list[Machine]) -> int:
     return fewest_buttons
 
 
+# note: after trying several ways of pruning the input space,
+# there wasn't an obvious solution besides an optimizer.
+# todo: come back and remove any dependencies if there is a good solution.
 def part2(machines: list[Machine]) -> int:
-    return 0
+    result = 0
+    for machine in machines:
+        buttons = machine.schematic
+        target = machine.joltages
+        num_buttons = len(buttons)
+        num_counters = len(target)
+        solver = z3.Optimize()
+
+        # each button is a variable
+        button_presses = [z3.Int(f"press_{j}") for j in range(num_buttons)]
+        for j in range(num_buttons):
+            # needs to be nonnegatives, not a normal system of linear equations
+            solver.add(button_presses[j] >= 0)
+
+        # for each counter i, create a vector that says the sum of these
+        # vectors must equal the target value
+        for i in range(num_counters):
+            vectors = [button_presses[j] for j in range(num_buttons) if i in buttons[j]]
+            solver.add(z3.Sum(vectors) == target[i])
+        total_presses = z3.Sum(button_presses)
+
+        # setting objective to be minimize total_presses
+        solver.minimize(total_presses)
+
+        # produce optimal values -> assuming we can solve all of these
+        solver.check()
+
+        model = solver.model()
+        min_presses = model.eval(total_presses).as_long()
+        result += min_presses
+    return result
 
 
 def main():
